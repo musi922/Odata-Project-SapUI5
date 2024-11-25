e79f9ab5c81d6875f3c8dae3777e6b9b28d7270e
musi922 <richardmusime6@gmail.com>
2024-11-25 09:19:16 +0200

    feat: fetched the products from northi wind

--- webapp/controller/Main.controller.js
+++ webapp/controller/Main.controller.js
@@ -1,7 +1,24 @@
-sap.ui.define(["./BaseController", "sap/m/MessageBox"], function (BaseController, MessageBox) {
+sap.ui.define([
+	"./BaseController", "sap/m/MessageBox","sap/ui/model/odata/v2/ODataModel"
+], function (BaseController, MessageBox,ODataModel) {
 	"use strict";
 
 	return BaseController.extend("com.myorg.myapp.controller.Main", {
+		onInit(){
+			var oModel = new ODataModel("http://localhost:3000/odata");
+this.getView().setModel(oModel);
+
+
+			oModel.read("/Products",{
+				success(odata){
+					console.log("Data Loaded Successfully",odata);
+				},
+				error(oError){
+					console.log("We faced Error",oError);
+					
+				}
+			})
+		},	
 		sayHello: function () {
 			MessageBox.show("Hello World!");
 		}
